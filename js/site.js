2420a2290f06535bd8a092119c20d9bc79892283
ADMIN <huydo164@gmail.com>
2020-12-25 16:21:33 +0700

    update

--- js/site.js
+++ js/site.js
@@ -1,30 +1,30 @@
-$(document).ready(function () {
-    var slideIndex;
 
-    function slideShow() {
-        var i ;
-        var slide = document.getElementsByClassName("slide-show");
-        var dot = document.getElementsByClassName("dot");
-        for (i = 0; i < slide.length; i++){
-            slide[i].style.display = "none";
-        }
-        for (i = 0; i < dot.length; i++){
-            dot[i].className = dot[i].className.replace(" active", "");
-        }
-        slide[slideIndex].style.display = "block";
-        dot[slideIndex].className += " active";
-        slideIndex++
-        if (slideIndex > slide.length - 1){
-            slideIndex = 0;
-        }
-        setTimeout(slideShow, 5000);
-    }
-    slideShow(slideIndex = 0);
 
-    function currentSlide(n) {
-        slideShow(slideIndex = n)
+var slideIndex;
+function slideShow() {
+    var i;
+    var slide = document.getElementsByClassName("slide-show");
+    var dot = document.getElementsByClassName("dot");
+    for (i = 0; i < slide.length; i++){
+        slide[i].style.display = "none";
     }
-})
+    for (i = 0; i < dot.length; i++){
+        dot[i].classList = dot[i].className.replace(" active", "");
+    }
+    slide[slideIndex].style.display = "block";
+    dot[slideIndex].className += " active";
+    slideIndex++;
+    if (slideIndex > slide.length - 1){
+        slideIndex = 0;
+    }
+    setTimeout(slideShow, 5000);
+}
+slideShow(slideIndex = 0);
+
+function currentSlide(n){
+    slideShow(slideIndex = n)
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
         var menu = document.querySelectorAll('header#wrapp-header');
@@ -38,7 +38,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 })
-
-
-
-
